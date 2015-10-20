09881ae428ae9e30632c61f1ae6094da88e5d45a
Alexandre Ktifa <alexandre.ktifa@gmail.com>
2015-10-20 15:50:06 +0200

    Setup dependencies

--- app.js
+++ app.js
@@ -1,17 +1,62 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var passport = require('passport');
+
+var session = require('express-session');
+var compression = require('compression');
+var cookieParser = require('cookie-parser');
+var cookieSession = require('cookie-session');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+var mongoStore = require('connect-mongo')(session);
+var flash = require('connect-flash');
+var helpers = require('view-helpers');
+
+var env = process.env.NODE_ENV || 'development';
 
 var app = express();
 var port = process.env.PORT || 3000;
 
 mongoose.connect('mongodb://localhost:27017/test');
 
-app.set('views', './app/views');
-app.set('view engine', 'jade');
+
+app.use(compression({ threshold: 512 }));
+
+require('./app/models/user.js');
+require('./config/passport')(passport);
 
 app.use('/assets', express.static(__dirname + './app/assets'));
 
-require('./app/models/user.js');
-require('./config/routes')(app);
+app.set('views', './app/views');
+app.set('view engine', 'jade');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride(function (req, res) {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    var method = req.body._method;
+    delete req.body._method;
+    return method;
+  }
+}));
+
+app.use(cookieParser());
+app.use(cookieSession({ secret: 'secret' }));
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: 'nodeflow',
+  store: new mongoStore({ url: 'mongodb://localhost:27017/test' })
+}));
+
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.use(helpers('nodeflow'));
+
+require('./config/routes')(app, passport);
 
 app.listen(port);
---
+++ config/passport.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
+
+var local = require('./passport/local');
+
+module.exports = function (passport) {
+  passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(function(id, done) {
+    User.load({ criteria: { _id: id } }, function (err, user) {
+      done(err, user);
+    });
+  });
+
+  passport.use(local);
+};
