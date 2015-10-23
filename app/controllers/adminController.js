ecd70b24ef570faffed7077b99d044a5e038c52c
Alexandre Ktifa <alexandre.ktifa@gmail.com>
2015-10-23 18:58:00 +0200

    Setup admin dashboard

--- app/controllers/adminController.js
+++ app/controllers/adminController.js
@@ -1,7 +1,15 @@
 var mongoose = require('mongoose');
 var User = mongoose.model('User');
+var Post = mongoose.model('Post');
 
 // Display admin dashboard
 exports.dashboard = function (req, res) {
-  res.render('admin/dashboard');
+  var counter = [];
+  User.count({}, function(err, count){
+    counter['users'] = count;
+    Post.count({}, function (err, count){
+      counter['posts'] = count;
+      res.render('admin/dashboard', { counter: counter });
+    });
+  });
 };
